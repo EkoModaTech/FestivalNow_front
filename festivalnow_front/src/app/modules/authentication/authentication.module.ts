1494fbc30d85eff679bf99938efc9cca0441e948
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-15 12:07:54 -0500

    implementacion interfaz login

--- festivalnow_front/src/app/modules/authentication/authentication.module.ts
+++ festivalnow_front/src/app/modules/authentication/authentication.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
@@ -21,7 +22,8 @@ import { ProviderComponent } from './provider/provider.component';
     ProviderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class AuthenticationModule { }
