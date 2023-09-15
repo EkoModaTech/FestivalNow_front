1aaaee2ca1827f870a359878dd2b8229d401a9b3
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-15 12:23:17 -0500

    implementacion recover password

--- festivalnow_front/src/app/app-routing.module.ts
+++ festivalnow_front/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { NotFoundComponent } from './home/not-found/not-found.component';
 import { LoginComponent } from './modules/authentication/login/login.component';
+import { RecoverPasswordComponent } from './modules/authentication/recover-password/recover-password.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'Home',pathMatch:'full'},
   {path: 'Home', component: HomeComponent},
   {path: 'not-found', component: NotFoundComponent },
-  {path: 'login',component: LoginComponent}
+  {path: 'login',component: LoginComponent},
+  {path: 'recover',component: RecoverPasswordComponent}
 ];
 
 @NgModule({
