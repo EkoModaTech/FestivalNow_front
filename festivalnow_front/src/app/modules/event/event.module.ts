91fcc07494b806e7e06c1231601f7c91046cc840
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-15 10:18:38 -0500

    cambios componentes eventos y solicitud eventos

--- festivalnow_front/src/app/modules/event/event.module.ts
+++ festivalnow_front/src/app/modules/event/event.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GeneralEventComponent } from './general-event/general-event.component';
 import { SpecificEventComponent } from './specific-event/specific-event.component';
+import { AdminEventComponent } from './admin-event/admin-event.component';
+import { ClientEventComponent } from './client-event/client-event.component';
+import { HostEventComponent } from './host-event/host-event.component';
 
 
 
 @NgModule({
   declarations: [
     GeneralEventComponent,
-    SpecificEventComponent
+    SpecificEventComponent,
+    AdminEventComponent,
+    ClientEventComponent,
+    HostEventComponent
   ],
   imports: [
     CommonModule
