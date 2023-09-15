d96ffe8035409db4198136630d345c40bc31fa71
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-15 16:59:25 -0500

    crud usuarios con edit role

--- festivalnow_front/src/app/modules/authentication/authentication.module.ts
+++ festivalnow_front/src/app/modules/authentication/authentication.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
@@ -8,7 +9,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { SettingComponent } from './setting/setting.component';
 import { ProviderComponent } from './provider/provider.component';
-
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 
 @NgModule({
@@ -19,11 +20,13 @@ import { ProviderComponent } from './provider/provider.component';
     ProfileComponent,
     TicketComponent,
     SettingComponent,
-    ProviderComponent
+    ProviderComponent,
+    EditUserComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule
   ]
 })
 export class AuthenticationModule { }
--- festivalnow_front/src/app/modules/authentication/setting/setting.component.ts
+++ festivalnow_front/src/app/modules/authentication/setting/setting.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { User } from 'src/app/models/user.models'; // Ajusta la ruta según la ubicación de tu modelo
+import { EditUserComponent } from '../edit-user/edit-user.component';
 
 
 @Component({
@@ -10,23 +13,61 @@ import { User } from 'src/app/models/user.models'; // Ajusta la ruta según la u
 
 
 export class SettingComponent {
+
   users: User[] = [
-    { id: 1, nombre: 'Usuario 1', rol: 'Rol 1' },
-    { id: 2, nombre: 'Usuario 2', rol: 'Rol 2' },
-    { id: 3, nombre: 'Usuario 3', rol: 'Rol 3' },
-    { id: 1, nombre: 'Usuario 1', rol: 'Rol 1' },
-    { id: 2, nombre: 'Usuario 2', rol: 'Rol 2' },
-    { id: 3, nombre: 'Usuario 3', rol: 'Rol 3' },
-    { id: 1, nombre: 'Usuario 1', rol: 'Rol 1' },
-    { id: 2, nombre: 'Usuario 2', rol: 'Rol 2' },
-    { id: 3, nombre: 'Usuario 3', rol: 'Rol 3' },
-    { id: 1, nombre: 'Usuario 1', rol: 'Rol 1' },
-    { id: 2, nombre: 'Usuario 2', rol: 'Rol 2' },
-    { id: 3, nombre: 'Usuario 3', rol: 'Rol 3' }
-  ];
+    { nombre: 'Usuario 1', rol: 'Rol 1' },
+    { nombre: 'Usuario 2', rol: 'Rol 2' },
+    { nombre: 'Usuario 3', rol: 'Rol 3' },
+    { nombre: 'Usuario 1', rol: 'Rol 1' },
+    { nombre: 'Usuario 2', rol: 'Rol 2' },
+    { nombre: 'Usuario 3', rol: 'Rol 3' },
+    { nombre: 'Usuario 1', rol: 'Rol 1' },
+    { nombre: 'Usuario 2', rol: 'Rol 2' },
+    { nombre: 'Usuario 3', rol: 'Rol 3' },
+    { nombre: 'Usuario 1', rol: 'Rol 1' },
+    { nombre: 'Usuario 2', rol: 'Rol 2' },
+    { nombre: 'Usuario 3', rol: 'Rol 3' }
+  ];  
+  usuarioEditando: User | null = null;
+  constructor(private dialog: MatDialog) {}
+
   editarUsuario(user: User) {
-    // Aquí puedes navegar a otro componente para editar el usuario
-    console.log('Editar usuario:', user);
+    // Abre el diálogo de edición
+    const dialogConfig = new MatDialogConfig();
+  
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    
+    dialogConfig.width = '40%';
+    // Personaliza la posición del diálogo
+    dialogConfig.position = {
+      left: '800px',
+      top: '-500px',
+    };
+  
+    // Personaliza el estilo del diálogo
+    dialogConfig.panelClass = 'custom-dialog-container';
+  
+    // Pasa el nombre de usuario y el rol al cuadro de diálogo
+    dialogConfig.data = { nombreUsuario: user.nombre, rolUsuario: user.rol };
+  
+    const dialogRef = this.dialog.open(EditUserComponent, dialogConfig);
+
+    // Escucha el cierre del diálogo y actualiza el usuario si se realizaron cambios
+    dialogRef.afterClosed().subscribe((result: User | undefined) => {
+      if (result) {
+        // Encuentra el índice del usuario editado en la lista
+        const index = this.users.findIndex(u => u === user);
+
+        if (index !== -1) {
+          // Reemplaza el usuario original con el usuario editado
+          this.users[index] = result;
+        }
+      }
+
+      // Limpia la variable usuarioEditando
+      this.usuarioEditando = null;
+    });
   }
   
   eliminarUsuario(user: User) {
@@ -37,4 +78,3 @@ export class SettingComponent {
     }
   }  
 }
-
