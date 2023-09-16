badd2ce0fc3d046a90a3525596f806b5f5d3ab3d
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-16 11:18:35 -0500

    Revert "crud usuarios con edit role"
    This reverts commit d96ffe8035409db4198136630d345c40bc31fa71.

--- festivalnow_front/src/app/models/user.models.ts
+++ festivalnow_front/src/app/models/user.models.ts
@@ -1,10 +1,8 @@
 export class User {
-  id: number;
   nombre: string;
   rol: string;
 
-  constructor(id: number, nombre: string, rol: string) {
-    this.id = id;
+  constructor(nombre: string, rol: string) {
     this.nombre = nombre;
     this.rol = rol;
   }
