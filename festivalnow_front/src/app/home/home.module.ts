f7cdf6edfab080c6fe8380c3620fd538a1757908
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-16 11:25:45 -0500

    componentes header y footer

--- festivalnow_front/src/app/home/home.module.ts
+++ festivalnow_front/src/app/home/home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
