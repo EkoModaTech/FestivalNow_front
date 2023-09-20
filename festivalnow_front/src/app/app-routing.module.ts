5c97088c65f94f016e745c5f1020a7cf5fafae73
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2023-09-19 20:42:27 -0500

    Added checkout page.

--- festivalnow_front/src/app/app-routing.module.ts
+++ festivalnow_front/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { NotFoundComponent } from './home/not-found/not-found.component';
 import { LoginComponent } from './modules/authentication/login/login.component';
+import { PseComponent } from './modules/buy/pse/pse.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'Home',pathMatch:'full'},
   {path: 'Home', component: HomeComponent},
+  {path: 'PSE', component: PseComponent},
   {path: 'not-found', component: NotFoundComponent },
   {path: 'login',component: LoginComponent}
 ];
