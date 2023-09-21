031f1f13df3c21ea1e65deae77d0a7d5bc71dcc0
Kevyn Prada <koprada@javeriana.edu.co>
2023-09-21 09:03:36 -0500

    eliminacion componentes innecesarios

--- festivalnow_front/src/app/home/home.module.ts
+++ festivalnow_front/src/app/home/home.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule }  from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
@@ -14,8 +12,6 @@ import { MatMenuModule } from '@angular/material/menu';
   declarations: [
     HomeComponent,
     NotFoundComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
