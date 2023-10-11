5bff132c6f0187b7e7f86a39e3016c310912760a
Kevyn <koprada@javeriana.edu.co>
2023-10-10 20:25:55 -0500

    Mejora interfaces

--- festivalnow_front/src/app/services/event.service.ts
+++ festivalnow_front/src/app/services/event.service.ts
@@ -2,23 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Event } from '../models/event.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+  private postEventApiUrl = environment.backendAPI + "/event/event/create";
+  private putEventApiUrl = environment.backendAPI + "/event/event/update/";
 
   constructor(private http: HttpClient) { }
 
   getEventos(): Observable<any[]> {
-    return this.http.get<any[]>(environment.backendAPI+"/event/event/list");
+    return this.http.get<any[]>(environment.backendAPI + "/event/event/list");
   }
 
-  createEvent(event: Event): Observable<Event> {
-    return this.http.post<Event>(this.postEventApiUrl, event)
+  createEvento(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.postEventApiUrl, event);
   }
 
-  updateEvent(event: Event): Observable<Event> {
-    return this.http.put<Event>(this.putEventApiUrl +event.idEvent, event)
+  updateEvento(event: Event): Observable<Event> {
+    return this.http.put<Event>(this.putEventApiUrl + event.idEvent, event);
   }
 }
