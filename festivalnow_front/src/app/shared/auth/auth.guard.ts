b711ac16a71b74ed630343f2f365c20a70bd503f
Kevyn Prada <koprada@javeriana.edu.co>
2023-10-18 23:32:12 -0500

    Implementacion manejo de roles

--- festivalnow_front/src/app/shared/auth/auth.guard.ts
+++ festivalnow_front/src/app/shared/auth/auth.guard.ts
@@ -11,11 +11,27 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.authService.isLoggedIn) {
-      return true; // Usuario autenticado, permite el acceso.
-    } else {
-      // Muestra una alerta en lugar de redirigir.
-      alert('Debes iniciar sesión para acceder a esta página.');
+      // Obtén el rol del usuario desde localStorage
+      const usuarioString = localStorage.getItem('usuario');
+      if (usuarioString !== null) {
+        const usuario = JSON.parse(usuarioString);
+        const userRoles: string[] = usuario.roles; // Especifica el tipo de datos de userRoles
 
+        // Define los roles necesarios para cada ruta
+        const requiredRoles: string[] = route.data['roles'] as string[]; // Especifica el tipo de datos de requiredRoles
+
+        // Verifica si el usuario tiene al menos uno de los roles necesarios para acceder a la ruta
+        if (userRoles.some((role: string) => requiredRoles.includes(role))) { // Especifica el tipo de datos de role
+          return true; // Usuario autenticado y autorizado, permite el acceso.
+        }
+      }
+      
+      // Usuario no tiene el rol necesario o no se pudo obtener el usuario, muestra un mensaje o redirige a una página de acceso no autorizado.
+      alert('No tienes permiso para acceder a esta página.');
+      return false;
+    } else {
+      // Usuario no autenticado, muestra una alerta en lugar de redirigir.
+      alert('Debes iniciar sesión para acceder a esta página.');
       // Evita que la navegación continúe.
       return false;
     }
--- festivalnow_front/src/app/shared/service/auth.service.ts
+++ festivalnow_front/src/app/shared/service/auth.service.ts
@@ -7,10 +7,10 @@ import { OIDCEntity } from '../model/oidc.entity';
 import jwtDecode from 'jwt-decode';
 
 interface MyToken {
-  resource_access: Object;
   email_verified: boolean;
   preferred_username: string;
   email: string;
+  realm_access: Record<string, any>;
 }
 
 @Injectable({
@@ -19,23 +19,35 @@ interface MyToken {
 export class AuthService {
   private authenticationResponseSubject: BehaviorSubject<OIDCEntity>
   public isLoggedIn: boolean = false;
+  public userRoles: string[] = [];
 
   public get isLogged() {
     return this.authenticationResponseSubject;
   }
 
   constructor(private http: HttpClient) {
-    let oidc = localStorage.getItem('currentOIDC')
+    let oidc = localStorage.getItem('currentOIDC');
     if (oidc === null) {
-      this.authenticationResponseSubject = new BehaviorSubject<OIDCEntity>(new OIDCEntity)
+      this.authenticationResponseSubject = new BehaviorSubject<OIDCEntity>(new OIDCEntity);
     } else {
-      this.authenticationResponseSubject = new BehaviorSubject<OIDCEntity>(JSON.parse(oidc))
+      this.authenticationResponseSubject = new BehaviorSubject<OIDCEntity>(JSON.parse(oidc));
+      this.setUserRoles(this.authenticationResponseSubject.value.access_token);
     }
   }
 
   public get token(): string {
-    return this.authenticationResponseSubject.value.access_token
+    return this.authenticationResponseSubject.value.access_token;
   }
+
+  // Decodificar el token JWT y obtener los roles desde realm_access
+  public setUserRoles(token: string): void {
+    const decodedToken = jwtDecode<MyToken>(token);
+    const roles = decodedToken.realm_access['roles']; 
+    if (roles) {
+      this.userRoles = roles;
+    }
+  }
+
 
   login(username: string, password: string) {
     return this.http.post<any>(`${environment.backendAPI}/user/auth/login`, {
@@ -47,12 +59,14 @@ export class AuthService {
         localStorage.setItem('currentOIDC', JSON.stringify(oidc));
 
         const jwtToken = oidc.access_token;
+        this.setUserRoles(jwtToken);
+
         const decodedToken = jwtDecode<MyToken>(jwtToken);
         const usuario = {
           username: decodedToken.preferred_username,
           email: decodedToken.email,
           email_verified: decodedToken.email_verified,
-          roles: decodedToken.resource_access
+          roles: decodedToken.realm_access['roles']
         }
 
         localStorage.setItem('usuario', JSON.stringify(usuario));
@@ -64,12 +78,12 @@ export class AuthService {
   }
 
   signup(user: RegisterRequest) {
-    return this.http.post(`${environment.backendAPI}/user/`, user, { responseType: 'text' })
+    return this.http.post(`${environment.backendAPI}/user/`, user, { responseType: 'text' });
   }
 
   logout() {
     const refreshToken = this.authenticationResponseSubject.value.refresh_token;
-  
+
     return this.http.post(`${environment.backendAPI}/user/auth/logout`, { refreshToken: refreshToken }).pipe(
       map(v => {
         this.removeToken();
@@ -78,14 +92,14 @@ export class AuthService {
       })
     );
   }
-  
 
   loginError() {
+    alert()
     this.authenticationResponseSubject.next(new OIDCEntity);
     this.isLoggedIn = false;
   }
 
   private removeToken() {
-    localStorage.removeItem("currentOIDC")
+    localStorage.removeItem("currentOIDC");
   }
 }
