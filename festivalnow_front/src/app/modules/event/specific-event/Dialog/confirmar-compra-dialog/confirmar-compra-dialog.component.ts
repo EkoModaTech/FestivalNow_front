6773e0492ebfdd5c39129b3347ca52fc85881f06
marioortiz6321 <marioortiz@javeriana.edu.co>
2023-11-19 18:44:42 -0500

    currency fixed

--- festivalnow_front/src/app/modules/event/specific-event/Dialog/confirmar-compra-dialog/confirmar-compra-dialog.component.ts
+++ festivalnow_front/src/app/modules/event/specific-event/Dialog/confirmar-compra-dialog/confirmar-compra-dialog.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TicketServiceService } from 'src/app/services/ticket-service.service';
 import { Ticket } from 'src/app/models/tickets.interface';
+
 @Component({
   selector: 'app-confirmar-compra-dialog',
   templateUrl: './confirmar-compra-dialog.component.html',
   styleUrls: ['./confirmar-compra-dialog.component.css']
 })
 export class ConfirmarCompraDialogComponent implements OnInit {
-  ticket: Ticket = {idTicket: null, code: null, price: null, idEvent: null}; // Inicializa ticket aquí
+  ticket: Ticket = {idTicket: null, code: null, price: null, idEvent: null};
 
   constructor(
     public dialogRef: MatDialogRef<ConfirmarCompraDialogComponent>,
@@ -20,7 +21,7 @@ export class ConfirmarCompraDialogComponent implements OnInit {
   }
 
   ngOnInit() {
-    const price = Math.random() * (100000 - 20000) + 20;
+    const price = Math.floor(Math.random() * (1000000 - 20000) + 20);
     this.ticketService.postTicket('A', price, this.data.eventId).subscribe((response: any) => {
       console.log(response);
       this.ticket = response as Ticket;
